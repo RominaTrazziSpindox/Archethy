6ed09f674764bec1a94155b1220f7b2e3d67a5e3
Romina Trazzi <romina.trazzi@spindox.it>
2025-12-17 11:55:17 +0100

    Postmapping

--- src/main/java/org/archethy/controllers/PersonaController.java
+++ src/main/java/org/archethy/controllers/PersonaController.java
@@ -1,9 +1,7 @@
 package org.archethy.controllers;
 
 import org.archethy.models.Persona;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +27,34 @@ public class PersonaController {
         elenco.add(new Persona("Marika", "Marico", 24));
         return elenco;
     }
+
+    /* GET MAPPING - METODO NON CORRETTO PER INVIARE INFORMAZIONI AL SERVER */
+    /* NON C'E' UN PAYLOAD, SOLO PARAMETRI O VARIABILI */
+
+    // Se non vengono inseriti tutti i parametri, oppure se i parametri non corrispondono al tipo indicato il server risponde 400: BadRequest
+    @GetMapping("/nuova")
+    public Persona nuovaPersona(
+                @RequestParam(name="nom") String varNome,
+                @RequestParam(name="cog") String varCognome,
+                @RequestParam(name="eta") int varEta
+            )
+    {
+        // Crea un oggetto di tipo Persona
+        Persona persona = new Persona(varNome, varCognome, varEta);
+
+        // Restituisci l'oggetto quando fai la chiamata
+        return persona;
+    }
+
+    /* POST MAPPING - METODO GIUSTO PER PER INVIARE INFORMAZIONI AL SERVER | SI PUO' TESTARE SOLO CON POSTMAN SE NON C'E' GUI */
+    /* RICHIESTE REQUESTBODY PER INVIARE PAYLOAD E SERIALIZZARE UN OGGETTO TRASFORMANDOLO IN JSON */
+
+    @PostMapping("/inserisci")
+    public Persona inserisciPersona(@RequestBody Persona persona) {
+        return persona;
+    }
+
+
+
 
 }
